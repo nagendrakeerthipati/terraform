a0552cdd102f9d88dc4abc73bb485240757c6926
nagendrakeerthipati <nagendravarma.nv@gmail.com>
2025-06-23 15:05:26 +0530

    practice

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -1,3 +1,12 @@
+resource "aws_instance" "roboshop" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
   description = "Allow all inbound traffic and all outbound traffic"
