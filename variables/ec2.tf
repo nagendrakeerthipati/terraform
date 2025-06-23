09794a5641bec3a3c5231fcb9fa6270c08834b4f
nagendrakeerthipati <nagendravarma.nv@gmail.com>
2025-06-23 17:51:52 +0530

    practice

---
+++ variables/ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "roboshop" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = var.tags
+}
+resource "aws_security_group" "allow_all" {
+  name        = var.sg_name
+  description = "Allow all inbound traffic and all outbound traffic"
+
+
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  tags = {
+    Name = "allow_all"
+  }
+}
